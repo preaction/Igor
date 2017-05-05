4ab1d956a7f070e6e5cfa99b16a9d9ba05c425b5
Doug Bell <doug@preaction.me>
2017-05-04 23:52:11 -0500

    add initial Beam::Service role
    This role indicates that an object can accept a `name` and `container`
    constructor argument when being created by Beam::Wire.

--- lib/Igor/Service.pm
+++ lib/Igor/Service.pm
@@ -1,19 +1,67 @@
 package Igor::Service;
 our $VERSION = '0.001';
-# ABSTRACT: Write a sentence about what it does
+# ABSTRACT: Role for services to access Igor features
 
 =head1 SYNOPSIS
 
+    package My::Object;
+    use Role::Tiny::With; # or Moo, or Moose
+    with 'Igor::Service';
+
+    package main;
+    use Igor;
+    my $wire = Igor->new(
+        config => {
+            my_object => {
+                '$class' => 'My::Object',
+            },
+        },
+    );
+
+    print $wire->get( 'my_object' )->name; # my_object
+
 =head1 DESCRIPTION
 
+This role adds extra functionality to an object that is going to be used
+as a service in a L<Igor> container. While any object can be
+configured with Igor, consuming the Igor::Service role allows an
+object to know its own name and to access the container it was
+configured in to fetch other objects that it needs.
+
 =head1 SEE ALSO
+
+L<Igor>
 
 =cut
 
 use strict;
 use warnings;
+use Moo::Role;
+use Types::Standard qw( Str InstanceOf );
 
+=attr name
 
+The name of the service. This is the name used in the L<Igor>
+configuration file for this service.
+
+=cut
+
+has name => (
+    is => 'ro',
+    isa => Str,
+);
+
+=attr container
+
+The L<Igor> container object that contained this service. Using
+this container we can get other services as-needed.
+
+=cut
+
+has container => (
+    is => 'ro',
+    isa => InstanceOf['Igor'],
+);
 
 1;
 
