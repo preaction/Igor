96b96a431ae1922e74ff4f2ec3ffc312333b3ec2
Doug Bell <doug@preaction.me>
2017-05-08 14:20:20 -0500

    clean up the docs just a bit more
    Calling them tasks makes it less confusing about which services/objects
    we're referring to.

--- lib/Igor/Runner.pm
+++ lib/Igor/Runner.pm
@@ -1,30 +1,30 @@
 package Igor::Runner;
 our $VERSION = '0.009';
-# ABSTRACT: Configure, list, document, and execute runnable objects
+# ABSTRACT: Configure, list, document, and execute runnable task objects
 
 =head1 SYNOPSIS
 
-    igor run <container> <service> [<args...>]
+    igor run <container> <task> [<args...>]
     igor list
     igor list <container>
-    igor help <container> <service>
+    igor help <container> <task>
     igor help
 
 =head1 DESCRIPTION
 
 This distribution is an execution and organization system for runnable
-objects. This allows you to prepare a list of runnable tasks in
+objects (tasks). This allows you to prepare a list of runnable tasks in
 configuration files and then execute them. This also allows easy
 discovery of configuration files and objects, and allows you to document
 your objects for your users.
 
-=head2 Container Files
+=head2 Configuration Files
 
 The configuration file is a L<Igor> container file that describes
-services. Some of these services are marked as executable by consuming the
-L<Igor::Runnable> role.
+objects. Some of these objects are marked as executable tasks by
+consuming the L<Igor::Runnable> role.
 
-The container file can have a special service called C<$summary> which
+The container file can have a special entry called C<$summary> which
 has a short summary that will be displayed when using the C<igor list>
 command.
 
@@ -51,12 +51,12 @@ documentation|Igor>.
 
 =head2 Tasks
 
-A task is an object configured in the container file. C<Igor::Runner>
-works with executable objects that consume the L<Igor::Runnable> role.
-This role requires only a C<run()> method be implemented in the class.
+A task is an object configured in the container file that consumes the
+L<Igor::Runnable> role. This role requires only a C<run()> method be
+implemented in the class.
 
-Tasks are expected to have documentation that will be displayed by
-the C<igor list> and C<igor help> commands. The C<igor list> command
+Task modules are expected to have documentation that will be displayed
+by the C<igor list> and C<igor help> commands. The C<igor list> command
 will display the C<NAME> section of the documentation, and the C<igor
 help> command will display the C<NAME>, C<SYNOPSIS>, C<DESCRIPTION>,
 C<ARGUMENTS>, C<OPTIONS>, C<ENVIRONMENT>, and C<SEE ALSO> sections of
