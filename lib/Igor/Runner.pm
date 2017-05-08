fe41c98f87fca0eac88c6825fb45842c7772197b
Doug Bell <doug@preaction.me>
2017-05-08 14:09:10 -0500

    clarify the project's use-case and goals
    Fixes #2

--- lib/Igor/Runner.pm
+++ lib/Igor/Runner.pm
@@ -1,6 +1,6 @@
 package Igor::Runner;
 our $VERSION = '0.008';
-# ABSTRACT: Execute runnable objects from Igor containers
+# ABSTRACT: Configure, list, document, and execute runnable objects
 
 =head1 SYNOPSIS
 
@@ -12,17 +12,17 @@ our $VERSION = '0.008';
 
 =head1 DESCRIPTION
 
-This distribution is an execution and organization system for
-L<Igor> containers. This allows you to prepare executable objects
-in configuration files and then execute them. This also allows easy
-discovery of container files and objects, and allows you to document
+This distribution is an execution and organization system for runnable
+objects. This allows you to prepare a list of runnable tasks in
+configuration files and then execute them. This also allows easy
+discovery of configuration files and objects, and allows you to document
 your objects for your users.
 
 =head2 Container Files
 
-The container file is a configuration file that describes services. Some
-of these services are marked as executable by using the L<Igor::Runnable>
-role.
+The configuration file is a L<Igor> container file that describes
+services. Some of these services are marked as executable by consuming the
+L<Igor::Runnable> role.
 
 The container file can have a special service called C<$summary> which
 has a short summary that will be displayed when using the C<igor list>
@@ -49,13 +49,13 @@ C<CPAN::Testers::Backend::Migrate::ToMetabase>:
 For more information about container files, see L<the Igor
 documentation|Igor>.
 
-=head2 Services
+=head2 Tasks
 
-A service is an object configured in the container file. C<Igor::Runner>
+A task is an object configured in the container file. C<Igor::Runner>
 works with executable objects that consume the L<Igor::Runnable> role.
 This role requires only a C<run()> method be implemented in the class.
 
-Services are expected to have documentation that will be displayed by
+Tasks are expected to have documentation that will be displayed by
 the C<igor list> and C<igor help> commands. The C<igor list> command
 will display the C<NAME> section of the documentation, and the C<igor
 help> command will display the C<NAME>, C<SYNOPSIS>, C<DESCRIPTION>,
