bd8cff14056d556bd4fb76c33f2902ce4c9e0c37
Doug Bell <doug@preaction.me>
2017-06-09 15:35:18 -0500

    rename RestrictUsers to AllowUsers for clarity
    This also will allow us to add a DenyUsers later so we can easily do
    "Only allow root to run this" and "Do not allow root to run this".

--- lib/Igor/Runnable/AllowUsers.pm
+++ lib/Igor/Runnable/AllowUsers.pm
@@ -1,34 +1,34 @@
-package Igor::Runnable::RestrictUsers;
+package Igor::Runnable::AllowUsers;
 our $VERSION = '0.012';
-# ABSTRACT: Restrict the users that can run a command
+# ABSTRACT: Only allow certain users to run a command
 
 =head1 SYNOPSIS
 
     ### In a Runnable module
     package My::Runnable::Script;
     use Moo;
-    with 'Igor::Runnable', 'Igor::Runnable::RestrictUsers';
-    has '+authorized_users' => ( default => [ 'root' ] );
+    with 'Igor::Runnable', 'Igor::Runnable::AllowUsers';
+    has '+allow_users' => ( default => [ 'root' ] );
     sub run { }
 
     ### In a container config file
     runnable:
         $class: My::Runnable::Script
         $with:
-            - 'Igor::Runnable::RestrictUsers'
-        authorized_users:
+            - 'Igor::Runnable::AllowUsers'
+        allow_users:
             - root
             - doug
 
 =head1 DESCRIPTION
 
-This role checks to ensure that only certain users can run a command. If an
-unauthorized user runs the command, it dies with an error instead.
+This role checks to ensure that only certain users can run a command. If
+an unauthorized user runs the command, it dies with an error instead.
 
 B<NOTE:> This is mostly a demonstration of a L<Igor::Runnable> role.
-Users that can write to the configuration file can edit who is
-authorized to run the command, and there are other ways to prevent
-access to a file/command.
+Users that can write to the configuration file can edit who is allowed
+to run the command, and there are other ways to prevent access to
+a file/command.
 
 =head1 SEE ALSO
 
@@ -42,13 +42,13 @@ use Moo::Role;
 use List::Util qw( any );
 use Types::Standard qw( ArrayRef Str );
 
-=attr authorized_users
+=attr allow_users
 
-An array reference of user names that are authorized to run this task.
+An array reference of user names that are allowed to run this task.
 
 =cut
 
-has authorized_users => (
+has allow_users => (
     is => 'ro',
     isa => ArrayRef[ Str ],
     required => 1,
@@ -65,7 +65,7 @@ before run => sub {
     my ( $self, @args ) = @_;
     my $user = getpwuid( $> );
     die "Unauthorized user: $user\n"
-        unless any { $_ eq $user } @{ $self->authorized_users };
+        unless any { $_ eq $user } @{ $self->allow_users };
 };
 
 1;
